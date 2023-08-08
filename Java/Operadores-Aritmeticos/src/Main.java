6199aa23a5c9bac0bf16aeab31e1b2f0169f27ba
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2023-08-08 15:54:23 -0300

    Java - ADD: Operadores-Logicos

---
+++ Java/Operadores-Aritmeticos/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+
+        int valor = 10 + 5;
+        int valor2 = 10 - 5;
+        int valor3 = 10 * 5;
+        int valor4 = 10 / 5;
+        int valor5 = 10 % 5;
+
+        System.out.println(valor);
+        System.out.println(valor2);
+        System.out.println(valor3);
+        System.out.println(valor4);
+        System.out.println(valor5);
+
+    }
+}
