4b176f73217f3167f389fef67e85d5c60bbfc38d
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2023-08-08 16:08:49 -0300

    'Java - ADD: Operadores - Logicos

---
+++ Java/Operadores-Logicos/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+
+                int valorDeX = 10;
+                int valorDeY = 20;
+
+                System.out.println(valorDeX > valorDeY);
+                System.out.println(valorDeX >= valorDeY);
+                System.out.println(valorDeX < valorDeY);
+                System.out.println(valorDeX <= valorDeY);
+                System.out.println(valorDeX == valorDeY);
+                System.out.println(valorDeX != valorDeY);
+            }
+        }
