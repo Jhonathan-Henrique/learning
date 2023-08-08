94bd40781d5965deaef6de4847caa3d292a522f0
Jhonathan Henrique <141654324+Jhonathan-Henrique@users.noreply.github.com>
2023-08-08 18:03:10 -0300

    Java - ATT: Adicionado 2 métodos de Array

--- Java/Array/Main.java
+++ Java/Array/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] familia = new String[4];
+        familia[0] = "Jhonathan";
+        familia[1] = "Vera";
+        familia[2] = "Ivanildo";
+        familia[3] = "Janny";
+
+        String[] familia2 = {
+            "Jhonathan",
+            "Vera",
+            "Ivanildo",
+            "Janny"
+        }
     }
 }
