d4eca19dace096f5590fe8b81db8b9e5076ab384
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2023-08-09 17:57:04 -0300

    Java - ADD: construtores

--- Java/orientacao a objetos/src/com/company/Carro.java
+++ Java/orientacao a objetos/src/com/company/Carro.java
@@ -2,20 +2,26 @@ package com.company;
 
 public class Carro {
 
-    //Atributos
+    //Atributos - caracteristicas
     public String nome;
     public String cor;
     public float peso;
     public int velocidade;
 
+    // construtor
+    public Carro(int velocidade, int tempo) {
+        this.movimentar(velocidade);
+        this.frear(tempo);
+    }
 
-    // Metodos
-    private void movimentar(int velocidade ) { //parametro dentro do metodo
-        System.out.println("O carro esta se movimentando com a velocidade: " + velocidade + " KM/H");
+
+    // Metodos - açoes
+    public void movimentar(int velocidade) { //parametro dentro do metodo
+        System.out.println("O carro esta se movimentando velocidade: " + velocidade + " km/h");
     }
 
     public void frear(int tempo) {
-        this.movimentar(85);
-        System.out.println("O carro freou em " + tempo + " segundos");
+        System.out.println("O carro freou" + tempo + "Segundos");
     }
+
 }
--- Java/orientacao a objetos/src/com/company/Main.java
+++ Java/orientacao a objetos/src/com/company/Main.java
@@ -3,11 +3,7 @@ package com.company;
 public class Main {
     public static void main(String[] args){
 
-        Carro gol = new Carro();
-        Carro fusca = new Carro();
-        Carro onix = new Carro();
+        Carro gol = new Carro(120,3);
 
-//        gol.movimentar(110);
-        gol.frear(4);
     }
 }
