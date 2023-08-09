e41c64136e20ed59805700e3078327d75457a3b2
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2023-08-09 17:24:01 -0300

    Java - ADD: Orientação a Objetos

---
+++ Java/orientacao a objetos/src/Carro.java
@@ -0,0 +1,18 @@
+public class Carro {
+
+    //Atributos
+    public String nome;
+    public String cor;
+    public float peso;
+    public int velocidade;
+
+
+    // Metodos
+    public void movimentar() {
+        System.out.println("O carro esta se movimentando");
+    }
+
+    public void frear() {
+        System.out.println("O carro freou.");
+    }
+}
---
+++ Java/orientacao a objetos/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+
+        Carro gol = new Carro();
+        Carro fusca = new Carro();
+        Carro onix = new Carro();
+
+        gol.movimentar();
+        gol.frear();
+    }
+}
