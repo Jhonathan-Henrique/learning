0d7fca78964765d6fdbec428b3f8c70a8b0b5ba7
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2023-08-15 09:46:12 -0300

    Android - ADD: EditText e como recuperar o dado digitado getText().toString()

---
+++ Android/componentes-EditeText/app/src/main/java/com/example/editetext/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.editetext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText edtNome = findViewById(R.id.edtNome);
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+        
+        btnEnviar.setOnClickListener(view -> {
+            Toast.makeText(this, edtNome.getText().toString(), Toast.LENGTH_SHORT).show();
+        });
+
+        
+        
+    }
+}
