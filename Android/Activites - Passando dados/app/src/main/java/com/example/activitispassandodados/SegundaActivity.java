d570eb1478312c720adbebdfe5ad604a8ee7d763
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2023-08-18 13:50:50 -0300

    Android - ADD: Passando dados entre Activitis

---
+++ Android/Activites - Passando dados/app/src/main/java/com/example/activitispassandodados/SegundaActivity.java
@@ -0,0 +1,33 @@
+package com.example.activitispassandodados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    private TextView textNome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        textNome = findViewById(R.id.text_nome);
+        String nome = getIntent().getStringExtra("nome");
+
+        textNome.setText(nome);
+
+        Button botao = findViewById(R.id.btn_terceiraActivitis);
+        botao.setOnClickListener(view -> {
+            Intent intent = new Intent(this, TerceiraActivity.class);
+            startActivity(intent);
+        });
+    }
+}
