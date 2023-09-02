bdcd5dbf7fa67d4ea9632825865b2bef0cdf4433
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2023-09-02 15:19:15 -0300

    Desenvolvimento Mobile - novo diretorio

---
+++ Mobile/Android/Activitys - Objetos entre Activitys/app/src/main/java/com/example/passandoobjetosentreactivitis/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.passandoobjetosentreactivitis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botao = findViewById(R.id.btn_segundaActivity);
+        botao.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SegundaActivity.class);
+            intent.putExtra("nome","Jhonathan");
+            startActivity(intent);
+        });
+    }
+}
