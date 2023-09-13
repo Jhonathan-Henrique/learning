e6e4bd2c0f78f37a4ce3646ab4d48783b5512625
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2023-09-13 15:23:37 -0300

    Python - ADD: Exemple input

--- Programming Logic/Python/Python (Testes).py
+++ Programming Logic/Python/Python (Testes).py
@@ -1,2 +1,16 @@
 # Primeiros exemplo em Python
-print("Hello World")
+print("Hello World, bem vindo aos meus testes de estudo em Python")
+
+#Comando input
+nome = input("Qual é o seu nome? ")
+print(nome)
+
+#Pegar o nome armazenado e dizer olá usando ele
+print(f"Olá {nome} seja bem vindo aos meus estudos de python")
+
+#Começa perguntando o nome do produto, quantidade em estoque e seu preço.
+nome = input("Qual o nome do produto? ")
+qtde = int(input("Qual a quantidade do produto? "))
+preco = float(input("Qual o valor do produto? "))
+
+print(f"O produto {nome} tem {qtde} unidades,no valor de R$ {preco} a unidade.")
