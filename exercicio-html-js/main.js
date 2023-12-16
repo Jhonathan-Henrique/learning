8ed53b653790b73102a7221a464ac35893c7a171
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2023-12-16 09:57:52 -0300

    Exercicio da Ebac HTML+CSS+JS

--- exercicio-html-js/main.js
+++ exercicio-html-js/main.js
@@ -3,10 +3,15 @@ const form = document.getElementById('form-validacao')
 function validaFormulario(){
     let numeroA = document.getElementById('numeroA')
     let numeroB = document.getElementById('numeroB')
+    if (numeroA.value < numeroB.value) { 
+        alert ('Seu formulario e valido') 
+    } else {
+        alert ('Seu formulario nao e valido')
+    }
 }
 
 form.addEventListener('submit', function(e){
-    e.preventDefault()
+    e.preventDefault();
 
-    
+    validaFormulario();
 })
